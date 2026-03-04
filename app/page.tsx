3629bde80bb9f71287c0a510a4af7ec99fe61a5d
Nyasha Hama <nyashahama45@gmail.com>
2026-03-04 21:04:07 +0200

    Integrate Demo component and useScrollAppear hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useScrollAppear } from "@/components/useScrollAppear";
 import Nav from "@/components/Nav";
 import Hero from "@/components/Hero";
 import Recognition from "@/components/Recognition";
@@ -5,27 +8,30 @@ import Benefits from "@/components/Benefits";
 import CrashTimeline from "@/components/CrashTimeline";
 import HowItWorks from "@/components/HowItWorks";
 import Score from "@/components/Score";
+import Demo from "@/components/Demo";
 import Testimonials from "@/components/Testimonials";
 import Pricing from "@/components/Pricing";
 import FinalCta from "@/components/FinalCta";
 import Footer from "@/components/Footer";
-import ScrollAnimation from "@/components/ScrollAnimation";
 
 export default function Home() {
+  useScrollAppear();
   return (
     <>
       <Nav />
-      <Hero />
-      <Recognition />
-      <Benefits />
-      <CrashTimeline />
-      <HowItWorks />
-      <Score />
-      <Testimonials />
-      <Pricing />
-      <FinalCta />
+      <main>
+        <Hero />
+        <Recognition />
+        <Benefits />
+        <CrashTimeline />
+        <HowItWorks />
+        <Score />
+        <Demo />
+        <Testimonials />
+        <Pricing />
+        <FinalCta />
+      </main>
       <Footer />
-      <ScrollAnimation />
     </>
   );
 }
